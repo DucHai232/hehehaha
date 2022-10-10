4203d05a3a33429ca815417b4d8c34a330c973cd
Duchai232 <Duchai232>
2022-10-10 23:48:11 +0700

    firtst status

---
+++ khunglong.js
@@ -0,0 +1,13 @@
+var love = document.getElementById("love");
+var no_love =document.getElementById("no-love");
+
+love.addEventListener("click", myFunction);
+function myFunction(){
+    
+ alert('Làm vợ anh nhaaa')
+}
+
+no_love.addEventListener("mouseover",lovefunction);
+function lovefunction(){
+   no_love.classList.toggle('move')
+}
